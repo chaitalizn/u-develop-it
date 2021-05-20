46b2c4f14674f3b877a640dec8cc736aa1faeed5
chaitalizn <phchaitali3@gmail.com>
2021-05-20 11:58:30 -0400

    connected to the MySQL Database and .env

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,23 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: process.env.DB_PW,
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 
 
